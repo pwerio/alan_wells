0e76e2612300a79bedf78be808f244b2aa3b65fe
PWER IO <eric@adlib.com.br>
2025-03-03 23:42:50 -0300

    Update map-section.tsx

--- components/map-section.tsx
+++ components/map-section.tsx
@@ -28,7 +28,7 @@ export default function MapSection() {
           </div>
           <div className="h-[400px] md:h-[500px] rounded-lg overflow-hidden">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.0976951624297!2d-46.65390548502264!3d-23.563273284682785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c8da0aa315%3A0xd59f9431f2c9776a!2sAv.%20Paulista%2C%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1647289523698!5m2!1spt-BR!2sbr"
+              src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.4047389611637!2d-46.6654168240931!3d-23.553902961319352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce582dcf13b031%3A0x2cdac5c041fad273!2sAv.%20Ang%C3%A9lica%2C%202530%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001228-200!5e0!3m2!1spt-BR!2sbr!4v1741056131385!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
               width="100%"
               height="100%"
               style={{ border: 0 }}
